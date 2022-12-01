65aeba2c11890ece51135a71266572691f0a6de1
Nathan West <Lucretiel@gmail.com>
2022-11-30 23:59:12 -0500

    Initial skeleton

---
+++ src/day1.rs
@@ -0,0 +1,7 @@
+pub fn part1(input: &str) -> anyhow::Result<i64> {
+    anyhow::bail!("solution not written yet");
+}
+
+pub fn part2(input: &str) -> anyhow::Result<i64> {
+    anyhow::bail!("solution not written yet");
+}
